899e5775d8066fad478f5701e71f2edbe0565279
bhupinder singh <bhupi12553@hotmail.com>
2019-02-25 14:34:32 -0500

    UPDATE

--- CSD2214-6-WEEK07/js/main.js
+++ CSD2214-6-WEEK07/js/main.js
@@ -1,1 +1,16 @@
 ﻿// Your code here!
+
+function total() {
+    var sum = 0;
+    for (var i = 117; i >= 11;)
+    {
+        
+        sum  += i;
+        i -= 8
+        
+    }
+    return sum;
+   
+}
+
+
